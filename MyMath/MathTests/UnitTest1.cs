a553e57026c80064745b94254e313ced6723e303
virginiayjd7 <37305214+virginiayjd7@users.noreply.github.com>
2020-07-06 02:19:21 -0500

    Parte 1: Creación de una prueba y generación del código

---
+++ MyMath/MathTests/UnitTest1.cs
@@ -0,0 +1,55 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMath;
+using System;
+
+namespace MathTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void RooterTestNegativeInputx()
+        {
+            Rooter rooter = new Rooter();
+            try
+            {
+                rooter.SquareRoot(-10);
+            }
+            catch (System.ArgumentOutOfRangeException)
+            {
+                return;
+            }
+            Assert.Fail();
+        }
+        public void RooterValueRange()
+        {
+            // Create an instance to test.
+            Rooter rooter = new Rooter();
+            // Try a range of values.
+            for (double expected = 1e-8; expected < 1e+8; expected *= 3.2)
+            {
+                RooterOneValue(rooter, expected);
+            }
+        }
+        private void RooterOneValue(Rooter rooter, double expectedResult)
+        {
+            double input = expectedResult * expectedResult;
+            double actualResult = rooter.SquareRoot(input);
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 1000);
+        }
+        public void TestMethod1()
+        {
+            // Create an instance to test:
+            Rooter rooter = new Rooter();
+            // Define a test input and output value:
+            double expectedResult = 2.0;
+            double input = expectedResult * expectedResult;
+            // Run the method under test:
+            double actualResult = rooter.SquareRoot(input);
+            // Verify the result:
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
+        }
+    }
+
+  
+}
